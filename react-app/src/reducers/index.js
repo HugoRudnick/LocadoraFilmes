184943b1468a4378c9e8fcd65d45a25f5253f4a0
HugoRudnick <hugoo_fr@hotmail.com>
2020-09-08 23:17:35 -0300

    Revert "Locadora"
    This reverts commit 69bede2d88145298a9e816ba12dd905abc5dbec5.

--- react-app/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-//import { usuario } from './usuario'
-// import { genero } from './genero'
-import { filme } from './filme'
-
-export const reducers = combineReducers({
-  filme
-})
